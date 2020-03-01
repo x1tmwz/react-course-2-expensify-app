ed472535f3943d7839936a6f69570c04fdf60da1
x1tmwz <585702289+x1tmwz@users.noreply.github.com>
2020-03-01 15:36:11 +0200

    setup expense summry compnent and test it

--- src/components/ExpenseDashBoard.js
+++ src/components/ExpenseDashBoard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilter from './ExpenseListFilter';
+import ExpensesSummary from './ExpensesSummary'
 
 const ExpenseDashBoard = () => (
     <div>
         <p>Home Page</p>
         <ExpenseListFilter />
+        <ExpensesSummary />
         <ExpenseList />
     </div>
 );
